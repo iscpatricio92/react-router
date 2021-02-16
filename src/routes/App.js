467ccf93309dff09fae6346032b9502ef8816b34
Patricio <Patricio@C02T14VNG8WN.local>
2021-02-15 18:28:19 -0600

    chance url remote to pat

--- src/routes/App.js
+++ src/routes/App.js
@@ -1,24 +1,24 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import Home from '../containers/Home'
-import Login from '../containers/Login'
-import Register from '../containers/Register'
-import NotFount from '../containers/NotFount'
-import Player from '../containers/Player'
-import Layout from '../components/Layout'
+import { BrowserRouter } from 'react-router-dom';
+import Home from '../containers/Home';
+import Login from '../containers/Login';
+import Register from '../containers/Register';
+import NotFount from '../containers/NotFount';
+import Player from '../containers/Player';
+import Layout from '../components/Layout';
 
 const App = () => (
-    <BrowserRouter>
-        <Layout>
-            <Switch>
-                <Route exact path='/' component={Home}></Route>
-                <Route exact path='/login' component={Login}></Route>
-                <Route exact path='/register' component={Register}></Route>
-                <Route exact path='/player/:id' component={Player}></Route>
-                <Route component={NotFount}></Route>
-            </Switch>
-        </Layout>
-    </BrowserRouter>
+  <BrowserRouter>
+    <Layout>
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route exact path='/login' component={Login} />
+        <Route exact path='/register' component={Register} />
+        <Route exact path='/player/:id' component={Player} />
+        <Route component={NotFount} />
+      </Switch>
+    </Layout>
+  </BrowserRouter>
 );
 
 export default App;
